4e8faee5a94a98b1f337ec149ac228a7aa080d4b
Richard Vu <richardvu.work@gmail.com>
2022-06-10 02:12:30 +0700

    Add env

---
+++ internet-banking-discovery-service/src/main/java/com/saigon/services/discovery/configuration/WebSecurity.java
@@ -0,0 +1,24 @@
+package com.saigon.services.discovery.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurity {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+
+    return http.build();
+  }
+
+}
